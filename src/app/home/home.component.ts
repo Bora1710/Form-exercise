847a37dd17008ab13e8764b72aaf5e55f037f979
Bora1710 <milosevicb92@gmail.com>
2023-08-05 19:28:58 +0200

    Add password to form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,8 +8,16 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  genderOptions = ['Male', 'Female', 'Other']
-  user = { name: '', gender: '', date: '', age: 0, contact: {phone: ''}, email: ''};
+  genderOptions = ['Male', 'Female', 'Other'];
+  user = {
+    name: '',
+    gender: '',
+    date: '',
+    age: 0,
+    contact: { phone: '' },
+    email: '',
+    password: '',
+  };
   maxDate: string;
   calculatedAge!: number;
 
@@ -19,17 +27,27 @@ export class HomeComponent {
     date: new FormControl(this.user.date, Validators.required),
     age: new FormControl(this.user.age),
     contact: new FormControl(this.user.contact.phone, Validators.required),
-    email: new FormControl(this.user.email, [Validators.required, Validators.email])
+    email: new FormControl(this.user.email, [
+      Validators.required,
+      Validators.email,
+    ]),
+    // Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$') email
+    password: new FormControl(this.user.password, [Validators.required, Validators.pattern("^(?=.*[A-Z])(?=.*?[0-9])[a-zA-Z0-9]{8,}$")]),
   });
-  
-  constructor () {let today = new Date();
-    let maxDate = new Date(today.getFullYear() - 12, today.getMonth(), today.getDate());
+
+  constructor() {
+    let today = new Date();
+    let maxDate = new Date(
+      today.getFullYear() - 12,
+      today.getMonth(),
+      today.getDate()
+    );
     this.maxDate = maxDate.toISOString().split('T')[0];
   }
   get name() {
     return this.userForm.get('name')!;
   }
-  
+
   get gender() {
     return this.userForm.get('gender')!;
   }
@@ -46,17 +64,24 @@ export class HomeComponent {
     return this.userForm.get('email');
   }
 
+  get password() {
+    return this.userForm.get('password');
+  }
+
   onDateChange(event: Event) {
     let selectedDate = (event.target as HTMLInputElement).value;
     let currentDate = new Date();
     let selected = new Date(selectedDate);
-    let ageDiff =  currentDate.getFullYear() - selected.getFullYear();
-    if (currentDate.getMonth() < selected.getMonth() ||
-        (currentDate.getMonth() === selected.getMonth() && currentDate.getDate() < selected.getDate())) {
+    let ageDiff = currentDate.getFullYear() - selected.getFullYear();
+    if (
+      currentDate.getMonth() < selected.getMonth() ||
+      (currentDate.getMonth() === selected.getMonth() &&
+        currentDate.getDate() < selected.getDate())
+    ) {
       this.calculatedAge = ageDiff - 1;
     } else {
       this.calculatedAge = ageDiff;
     }
     this.userForm.controls.age.setValue(this.calculatedAge);
-}
+  }
 }
