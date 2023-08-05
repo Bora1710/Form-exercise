a20ba992b7b6c7ff1ba0ba464c0cfd95dd814f34
Bora1710 <milosevicb92@gmail.com>
2023-08-05 16:40:26 +0200

    Add date element to form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,17 +9,28 @@ import { Validators } from '@angular/forms';
 })
 export class HomeComponent {
   genderOptions = ['Male', 'Female', 'Other']
-  user = { name: '', gender: ''};
+  user = { name: '', gender: '', date: ''};
+  maxDate: string;
+
   userForm = new FormGroup({
     name: new FormControl(this.user.name, Validators.required),
-    gender: new FormControl(this.user.gender, Validators.required)
+    gender: new FormControl(this.user.gender, Validators.required),
+    date: new FormControl(this.user.date, Validators.required)
   });
-
+  
+  constructor () {const today = new Date();
+    const maxDate = new Date(today.getFullYear() - 12, today.getMonth(), today.getDate());
+    this.maxDate = maxDate.toISOString().split('T')[0];
+  }
   get name() {
     return this.userForm.get('name')!;
   }
   
   get gender() {
     return this.userForm.get('gender')!;
+  }
+
+  get date() {
+    return this.userForm.get('date')!;
   }
 }
