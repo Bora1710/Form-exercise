b03e8f68288a8a4a3c1a42ced2be46bda805c398
Bora1710 <milosevicb92@gmail.com>
2023-08-05 15:29:46 +0200

    Delete all unnecessary elements and add styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,20 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
-import { User } from '../Instances/newUser';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-  userForm = new FormGroup({
-    name: new FormControl<string>('', Validators.required),
-    gender: new FormControl<string>('', Validators.required),
-    date: new FormControl<String>('', Validators.required),
-    age: new FormControl<number>(0, Validators.required),
-    contact: new FormControl<string>('', Validators.required),
-    email: new FormControl<string>('', Validators.required),
-    password: new FormControl<string>('', Validators.required),
-    repeatPassword: new FormControl<string>('', Validators.required),
-    hobies: new FormControl('', Validators.required),
-  })
 
-  // confirm() {
-  //   let user = new User(this.userForm.value);
-  // }
+
+export class HomeComponent {
+  user = {name: ""};
+  userForm = new FormGroup({
+    name: new FormControl(this.user.name, Validators.required),
+  });
+
+  get name() { return this.userForm.get('name')!; }
 }
