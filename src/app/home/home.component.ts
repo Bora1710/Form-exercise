20c101dfdc7b41ba74b3b9a27433caa1ab814210
Bora1710 <milosevicb92@gmail.com>
2023-08-05 15:50:30 +0200

    Add gender options to user form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,19 +2,24 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-
-
 export class HomeComponent {
-  user = {name: ""};
+  genderOptions = ['','Male', 'Female', 'Other']
+  user = { name: '', gender:  this.genderOptions[0]};
   userForm = new FormGroup({
     name: new FormControl(this.user.name, Validators.required),
+    gender: new FormControl(this.user.gender, Validators.required)
   });
 
-  get name() { return this.userForm.get('name')!; }
+  get name() {
+    return this.userForm.get('name')!;
+  }
+  
+  get gender() {
+    return this.userForm.get('gender')!;
+  }
 }
